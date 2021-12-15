af87062e55f39dd1d841b9587cdb9be595cf5678
lovecoding-git <59399614+carolyncoleman2020@users.noreply.github.com>
2021-12-15 10:15:23 -0500

    commit

---
+++ docs/linux.md
@@ -0,0 +1,10 @@
+#### Commands
+- unzip `unzip [FILE_NAME]`
+- rename file `mv [FILE_NAME_1] [FILE_NAME_2]`
+
+#### Installations
+- install zip
+```
+sudo apt install zip
+```
+
--- src/components/pageList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Loading from 'components/Loading';
-import loadable from 'utils/loadable';
-
-
-export const HomePage = loadable(()=>import('pages/Home'),{
-    fallback: <Loading/>
-})
